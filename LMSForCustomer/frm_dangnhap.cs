d3e38d3f74e01624ca6b4d3c1819c0a7556a2441
DinhGH <huynhdinh2k52707@gmail.com>
2024-12-05 21:08:51 +0700

    è

---
+++ LMSForCustomer/frm_dangnhap.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace LMSForCustomer
+{
+    public partial class frm_dangnhap : DevExpress.XtraEditors.XtraForm
+    {
+        public frm_dangnhap()
+        {
+            InitializeComponent();
+        }
+
+        private void hyperlinkLabelControl2_Click(object sender, EventArgs e)
+        {
+            frm_dangky frm = new frm_dangky();
+            frm.Show();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+           Trangchu frm = new Trangchu();
+            frm.Show();
+        }
+    }
+}
